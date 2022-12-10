0bd670c144962d56ad5ee760e0ba3747aaee95d4
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-10 15:43:13 -0800

    add thought form and functionality added

--- server/models/Thought.js
+++ server/models/Thought.js
@@ -6,7 +6,7 @@ const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: 'You need to leave a thought!',
+      required: true,
       minlength: 1,
       maxlength: 280
     },
